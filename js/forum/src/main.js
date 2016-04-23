1295bc36b5eb66a5cb40e99059a85de2e72cf1c3
Connor <connor0095@gmail.com>
2016-04-22 20:50:43 -0400

    Fix Static 'Loading' and only replacing one issue

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -7,8 +7,8 @@ app.initializers.add('davis-securehttps-forum', function() {
         this.imageonerror = "$(this).next().empty().append('<blockquote style=&#92;'background-color: #c0392b; color: white;&#92;' class=&#92;'uncited&#92;'><div><p>"+app.translator.trans('davis-securehttps.forum.removed')+"</p></div></blockquote>');$(this).hide();";
         this.props.post.contentHtml = m.prop(
             this.props.post.contentHtml().replace(
-                /<img src="http:\/\/(.+)" title="(.*)" alt="(.*)">/,
-                '<img onerror="'+this.imageonerror+'" class="securehttps-replaced" src="https://$1" title="$2" alt="$3"><span><br><br><i class="icon fa fa-spinner fa-spin fa-3x fa-fw"></i> Loading Image<br></span>'
+                /<img src="http:\/\/(.+)" title="(.*)" alt="(.*)">/g,
+                '<img onerror="'+this.imageonerror+'" onload="$(this).next().empty();" class="securehttps-replaced" src="https://$1" title="$2" alt="$3"><span><br><br><i class="icon fa fa-spinner fa-spin fa-3x fa-fw"></i> Loading Image<br></span>'
             )
         );
     });
